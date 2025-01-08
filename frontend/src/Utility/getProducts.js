d1c44d54cb4028ed72205b9826ca778a06ab7af7
AliHamza-Coder <ali@123@hamza>
2025-01-09 04:44:06 +0500

    commit

--- frontend/src/Utility/getProducts.js
+++ frontend/src/Utility/getProducts.js
@@ -1,25 +1,23 @@
-
 export const fetchProducts = async () => {
-    const headers = {
-      Authorization:
-        "Bearer 8888f5b33a2b554d9f36b46a1413eae10874e0c6a3547e313d69da6d1414d4e33c2bec377a4cb33e63a9b70aeef35830deac04af2058338b0cb56630f739b8f54fed07ef760f4a29bf3026a51ce3950eef91c3b18e65829210431ac4ba4341e5cd1ac9309522b5e9d1b943aa44dc8dbdeb039cbf264de7f0acea51b841da48ef",
-    };
-  
-    const response = await fetch("http://localhost:1337/api/products?populate=*", {
-      headers,
-    });
-  
-    if (!response.ok) {
-      throw new Error("Failed to fetch products");
-    }
-  
-    const data = await response.json();
-    return data.data.map((item) => ({
-      id: item.id,
-      title: item.title || "No Title",
-      description: item.description || "No Description",
-      imageUrl: item.imageUrl || "/default-image.jpg",
-      price: item.price || 0,
-    }));
+  const headers = {
+    Authorization: process.env.NEXT_PUBLIC_AUTH_KEY,
   };
-  
+
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/products?populate=*`,
+    { headers }
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch products");
+  }
+
+  const data = await response.json();
+  return data.data.map((item) => ({
+    id: item.id,
+    title: item.title || "No Title",
+    description: item.description || "No Description",
+    imageUrl: item.imageUrl || "/default-image.jpg",
+    price: item.price || 0,
+  }));
+};
